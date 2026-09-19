4732279b21132b4a59edc70f98802d378267ec03
Giselle Gallegos <gisellegallegos@outlook.com>
2026-09-18 23:25:40 -0600

    Actividad PopupUI al 100

---
+++ Assets/Scripts/UIWindow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class UIWindow : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    public virtual void Show()
+    {
+
+    }
+
+    public virtual void Hide()
+    {
+
+    }
+
+ 
+}

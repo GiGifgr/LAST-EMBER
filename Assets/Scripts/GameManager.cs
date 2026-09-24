c3a256fc0a14735851ce304565a0009bf225fd41
Giselle <gisellegallegos@outlook.com>
2026-09-23 20:45:23 -0600

    todos los scripts ya

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using System;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public UIManager UIManager;
+    public string uiToShow;
+
+    [Button]
+    private void Test()
+    {
+        UIManager.ShowWindow(uiToShow);
+    }
+
+}
--- Assets/Scripts/UIWindow.cs
+++ Assets/Scripts/UIWindow.cs
@@ -1,26 +1,39 @@
 using DG.Tweening;
-using UnityEngine;
 using NaughtyAttributes;
+using UnityEngine;
 
 public class UIWindow : MonoBehaviour
 {
+    [Header("Data")]
+    [SerializeField] private string _id;
+    [Header("UI Settings")]
     [SerializeField] private RectTransform _canvasRectTransform;
     [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private bool _hideOnStart;
+
+    [Header("Animation Settings")]
+    [SerializeField] private float showDuration = 0.5f;
+    [SerializeField] private float hideDuration = 0.5f;
+
+    [SerializeField] private Ease showEase = Ease.OutBack;
+    [SerializeField] private Ease hideEase = Ease.InBack;
+
+    public CanvasGroup CanvasGroup => _canvasGroup;
+    public RectTransform CanvasRectTransform => _canvasRectTransform;
+    public string Id => _id;
+
     void Start()
     {
-        Initalize();
+        Initialize();
     }
 
-    public virtual void Initalize()
+    public virtual void Initialize()
     {
         if (_hideOnStart)
         {
-            Hide();
+            Hide(true);
         }
     }
-
-    [Button]
     public virtual void Show(bool instant = false)
     {
         if (instant)
@@ -29,12 +42,12 @@ public class UIWindow : MonoBehaviour
         }
         else
         {
+            _canvasRectTransform.gameObject.SetActive(true);
             RectTransform rectTransform = _canvasGroup.GetComponent<RectTransform>();
-            rectTransform.DOScale(Vector3.one, duration: 0.5f).SetEase(Ease.OutBack);
+            rectTransform.DOScale(Vector3.one, showDuration).SetEase(showEase);
         }
     }
 
-    [Button]
     public virtual void Hide(bool instant = false)
     {
         if (instant)
@@ -44,7 +57,12 @@ public class UIWindow : MonoBehaviour
         else
         {
             RectTransform rectTransform = _canvasGroup.GetComponent<RectTransform>();
-            rectTransform.DOScale(Vector3.zero, duration: 0.5f).SetEase(Ease.InBack);
+            rectTransform.DOScale(Vector3.zero, hideDuration).SetEase(hideEase).OnComplete(() =>
+            {
+                _canvasRectTransform.gameObject.SetActive(false);
+            });
         }
     }
+
+
 }
